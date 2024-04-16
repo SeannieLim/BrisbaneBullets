1fdfcba3f79b2575e965be6efc84cf81564cead7
Arinexus <arin.ning02@gmail.com>
2024-04-16 23:05:37 +1000

    Toggle button

--- BrisbaneBullets/App.js
+++ BrisbaneBullets/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <HomeScreen />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ BrisbaneBullets/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { VStack } from "native-base";
+import ToggleButton from "../components/ToggleButton";
+
+const HomeScreen = () => {
+  return (
+    <VStack space={4} alignItems="center">
+      <ToggleButton />
+    </VStack>
+  );
+};
+
+export default HomeScreen;
