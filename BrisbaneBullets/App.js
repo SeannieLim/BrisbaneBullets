59cd7c54726bcee1a94fbc130c31a74a40648349
tracychiang25 <hsinyen19@gmail.com>
2024-04-22 20:35:21 +1000

    Update App.js
    create social media buttons

--- BrisbaneBullets/App.js
+++ BrisbaneBullets/App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, ScrollView, View, Image, TouchableOpacity, Linking } from 'react-native';
+//import { Button, ButtonText, ButtonIcon,} from "@gluestack-ui/themed"
+
 
 export default function App() {
+
+  const socials = [
+    { img: require('./assets/facebook.png'), url:'https://www.facebook.com/BrisbaneBullets/', webUrl: 'https://www.facebook.com/BrisbaneBullets/'}, 
+    { img: require('./assets/X.png'),  url:'https://twitter.com/BrisbaneBullets', webUrl:'https://twitter.com/BrisbaneBullets'}, 
+    { img: require('./assets/Instagram.png'),  url:'https://www.instagram.com/brisbanebullets/', webUrl:'https://www.instagram.com/brisbanebullets/'}, 
+    { img: require('./assets/Youtube.png'), url:'https://www.youtube.com/channel/UCJaGGZG2kamXywgks_xWx0A', webUrl:'https://www.youtube.com/channel/UCJaGGZG2kamXywgks_xWx0A'}]
+
+    const handleClick = async (social) => {
+      const supported = await Linking.canOpenURL(social.url);
+      if (supported) {
+        Linking.openURL(social.url)
+          .catch(err => console.error('An error occurred', err));
+      } else {
+        Linking.openURL(social.webUrl)
+          .catch(err => console.error('An error occurred', err));
+      }
+
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {socials.map((social, index) => (
+          <TouchableOpacity key={index} style={styles.button} onPress={() => handleClick(social)}>
+            {/*<Button size="lg"
+              variant="solid"
+              action="primary"
+              isDisabled={false}
+              isFocusVisible={false}
+              >*/}
+              <Image source={social.img} style={styles.img} resizeMode="contain" />
+            {/*</Button>*/}
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
+
+
   );
 }
 
@@ -17,4 +52,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  button: {
+    width: 95,
+    height: 95,
+  },
+
+  img: {
+    flex: 1,
+    width: 90,
+    height: 70,
+}
 });
