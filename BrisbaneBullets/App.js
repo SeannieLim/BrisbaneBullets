d1927806d440b95bef2f801c5b88dd62bc335c3f
tracychiang25 <hsinyen19@gmail.com>
2024-04-29 19:43:23 +1000

    creating boxes

--- BrisbaneBullets/App.js
+++ BrisbaneBullets/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.boxContainer}>
+        <View style={[styles.topBox, { backgroundColor: '#164CA8' }]} />
+        <View style={[styles.belowBox, { backgroundColor: '#164CA8' }]} />
+      </View>
+      <View style={styles.boxContainer}>
+        <View style={[styles.topBox, { backgroundColor: '#164CA8' }]} />
+        <View style={[styles.belowBox, { backgroundColor: '#164CA8' }]} />
+      </View>
     </View>
   );
 }
@@ -13,8 +19,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  boxContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topBox: {
+    width: 260,
+    height: 30,
+    marginVertical: 0.3,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  belowBox: {
+    width: 260,
+    height: 30,
+    marginVertical: 0.3,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
   },
 });
