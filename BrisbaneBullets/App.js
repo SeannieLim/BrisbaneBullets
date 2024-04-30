59cb096a3004fafe09661f6fc58e213b81751c12
tracychiang25 <hsinyen19@gmail.com>
2024-04-30 17:22:25 +1000

    change to button

--- BrisbaneBullets/App.js
+++ BrisbaneBullets/App.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import {
+  Button,
+  ButtonText,
+  ButtonIcon,
+  ButtonSpinner,
+  ButtonGroup,
+} from "@gluestack-ui/themed"
+
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.boxContainer}>
-        <View style={[styles.topBox, { backgroundColor: '#164CA8' }]} />
-        <View style={[styles.belowBox, { backgroundColor: '#164CA8' }]} />
+      <View style={[styles.boxContainer, styles.marginBottom]}>
+          <Button style={[styles.topButton, { backgroundColor: '#164CA8' }]}>
+            <ButtonText style={styles.buttonText}>Push Notification </ButtonText>
+          </Button>
+          <Button style={[styles.bottomButton, { backgroundColor: '#164CA8' }]}>
+            <ButtonText style={styles.buttonText}>Membership </ButtonText>
+          </Button>
       </View>
-      <View style={styles.boxContainer}>
-        <View style={[styles.topBox, { backgroundColor: '#164CA8' }]} />
-        <View style={[styles.belowBox, { backgroundColor: '#164CA8' }]} />
+      <View style={[styles.boxContainer, styles.marginTop]}>
+        <Button style={[styles.topButton, { backgroundColor: '#164CA8' }]}>
+          <ButtonText style={styles.buttonText}>Privacy Policy</ButtonText>
+          <ButtonIcon
+          as={}
+          />
+        </Button>
+        <Button style={[styles.midButton, { backgroundColor: '#164CA8' }]}>
+          <ButtonText style={styles.buttonText}>Feedback and Support </ButtonText>
+        </Button>
+        <Button style={[styles.bottomButton, { backgroundColor: '#164CA8' }]}>
+          <ButtonText style={styles.buttonText}>Terms and Condition </ButtonText>
+        </Button>
       </View>
     </View>
+
+
   );
 }
 
@@ -28,18 +55,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  topBox: {
+  topButton: {
     width: 260,
     height: 30,
     marginVertical: 0.3,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    justifyContent: 'center', 
+
   },
-  belowBox: {
+  midButton:{
+    width: 260,
+    height: 30,
+    marginVertical: 0.3,
+  },
+
+  bottomButton: {
     width: 260,
     height: 30,
     marginVertical: 0.3,
     borderBottomLeftRadius: 15,
     borderBottomRightRadius: 15,
+  },
+  buttonText: {
+    color: 'white', 
+    fontSize: 16,
+    paddingLeft: 15,
+  },
+  marginTop: {
+    marginTop: 10, 
+  },
+  marginBottom: {
+    marginBottom: 10, 
   },
 });
