c0cb82ec40e7c463fffa80d6a7f56f07f9ed58fc
Cody Khor <codykhor@gmail.com>
2024-05-02 16:46:12 +1000

    revert

--- BrisbaneBullets/src/components/ToggleButton.js
+++ BrisbaneBullets/src/components/ToggleButton.js
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25, // Shadow opacity
     shadowRadius: 4, // Shadow radius
     elevation: 5, // Elevation for Android
-    backgroundColor: "red",
+    backgroundColor: "#FFFFFF",
   },
 });
 
