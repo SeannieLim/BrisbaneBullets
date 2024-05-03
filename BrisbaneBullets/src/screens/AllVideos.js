a336fff72d73136c0627eed3857bf370a15f98e3
Tracy C <hsinyen19@gmail.com>
2024-05-03 19:46:30 +1000

    Update AllVideos

--- BrisbaneBullets/src/screens/AllVideos.js
+++ BrisbaneBullets/src/screens/AllVideos.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import { Box,  VStack, Image } from "@gluestack-ui/themed";
+import {React, useState } from 'react';
+import { StyleSheet, TouchableOpacity, Linking, ImageBackground, Text, Dimensions } from 'react-native';
+import { Box,  VStack, View } from "@gluestack-ui/themed";
+import {scaleFontSize} from "../constants/Layout";
+
+const windowWidth = Dimensions.get("window").width;
+const mockVideos=[
+{
+  id: 6,
+  title: 'NBL24 MVP Awards Gala winners announced',
+  date: 'Mar 28, 2024',
+  img: require('../../assets/News/newsImg2.png'),
+  imgAlt: 'newsImage'
+},
+{
+  id: 7,
+  title: 'Zikarsky to compete at Albert Schweitzer Tournament..',
+  date: 'Mar 21,2024',
+  img: require('../../assets/News/newsImg3.png'),
+  imgAlt: 'newsImage'
+},]
+
 
 const VideoBox = () =>{
     const [videos, setVideos] =useState([]);
+    const onPress={handlePress}
     const handlePress=()=>{
         const url = "https://www.youtube.com/watch?v=KvN8U6cqiu0&ab_channel=PewDiePie"
         Linking.openURL(url)
     }
 
     return(
-    <Box style={styles.box}> 
+    <View style={styles.box}> 
+        <Box style={styles.header}>
+          <Text style={styles.heading}>Videos</Text>
+        </Box>
       <VStack space="none" reversed={false} style={styles.vstack}>
-      <TouchableOpacity onPress={handlePress}>
-        <Image source={require('../Logo/BB-logo.png')} style={styles.video}/>
+      <TouchableOpacity onPress={handlePress} style={styles.video}>
+      <ImageBackground
+        source={require('../../assets/News/newsImg7.png')}
+        style={styles.imageBackground}
+      />
       </TouchableOpacity >
         <Box style={styles.description} />
       </VStack>
-    </Box>
+    </View>
 
     );
 
@@ -38,8 +64,26 @@ const styles = StyleSheet.create({
 
     box:{
         height:"100%",
-        justifyContent: "center",
+        // justifyContent: "center",
         backgroundColor:"pink",
+    },
+    header: {
+      paddingTop: windowWidth * 0.1,
+      paddingBottom: windowWidth *0.08,
+      flexDirection: 'row',
+      justifyContent:'center'
+  },
+    heading: {
+      color: '#113B81',
+      fontWeight:'600',
+      fontSize:scaleFontSize(29)
+    },
+    imageBackground: {
+      width: "100%",
+      height: "100%",
+      overflow: 'hidden',
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
     },
     vstack: {
         alignItems: "center", 
@@ -50,7 +94,7 @@ const styles = StyleSheet.create({
         height:"35%",
         borderTopLeftRadius: 15,
         borderTopRightRadius: 15,
-        backgroundColor:"yellow",
+        backgroundColor:"#164CA8",
 
     },
     description:{
