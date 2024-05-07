5e2ca032a1776f9cf3fc40b32239f8d2a7fd5303
Arinexus <arin.ning02@gmail.com>
2024-05-07 14:09:45 +1000

    used ayncstorage for add, delete and read notification

--- BrisbaneBullets/src/notifications/notificationContext.js
+++ BrisbaneBullets/src/notifications/notificationContext.js
@@ -1,4 +1,10 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import uuid from "react-native-uuid"; // Import the UUID generator
@@ -19,42 +25,98 @@ export const NotificationProvider = ({ children }) => {
   const [isEnabled, setIsEnabled] = useState(false);
   const [pushToken, setPushToken] = useState(null);
 
-  // Register for push notifications and handle received notifications
   useEffect(() => {
-    const fetchSettingsAndRegister = async () => {
-      const localSetting = await AsyncStorage.getItem("notificationsEnabled");
-      const isEnabledLocally = localSetting !== "false";
-      setIsEnabled(isEnabledLocally);
-      if (isEnabledLocally) {
-        await registerForPushNotificationsAsync();
-      }
-    };
-    fetchSettingsAndRegister();
+    loadInitialSettings();
   }, []);
 
-  // Notification listener with direct check to `AsyncStorage`
   useEffect(() => {
     const subscription = Notifications.addNotificationReceivedListener(
-      async (notification) => {
-        const localSetting = await AsyncStorage.getItem("notificationsEnabled");
-        const isEnabledLocally = localSetting !== "false";
-
-        if (!isEnabledLocally) {
-          console.log(
-            "Notification received but notifications are disabled locally"
-          );
-          return;
-        }
-
-        console.log("Notification received!");
-        addNotification(notification.request.content.title, notification.date);
-      }
+      handleNotificationReceived
     );
+    return () => subscription.remove();
+  }, [isEnabled]);
 
-    return () => {
-      Notifications.removeNotificationSubscription(subscription);
+  const loadInitialSettings = async () => {
+    const storedNotifications = await AsyncStorage.getItem("notifications");
+    const localSetting = await AsyncStorage.getItem("notificationsEnabled");
+    setIsEnabled(localSetting !== "false");
+    setNotifications(
+      storedNotifications ? JSON.parse(storedNotifications) : []
+    );
+    if (localSetting !== "false") {
+      await registerForPushNotificationsAsync();
+    }
+  };
+
+  const handleNotificationReceived = (notification) => {
+    if (isEnabled) {
+      addNotification(
+        notification.request.content.title,
+        notification.date || Date.now()
+      );
+    } else {
+      console.log(
+        "Notification received but notifications are disabled locally"
+      );
+    }
+  };
+
+  const addNotification = async (title, rawTimeStamp) => {
+    console.log("Received raw timeStamp:", rawTimeStamp);
+
+    // Determine if the timestamp is in seconds (iOS) or milliseconds (Android), ensure the timestamp is in milliseconds
+    const timeStamp =
+      rawTimeStamp > 1000000000000 ? rawTimeStamp : rawTimeStamp * 1000;
+
+    const actualDate = new Date(timeStamp);
+    console.log("Normalized Date:", actualDate);
+
+    const displayTimeStamp = (actualDate) => {
+      const now = Date.now();
+      const diffMs = now - actualDate.getTime(); // difference in milliseconds
+      const diffSec = diffMs / 1000;
+      const diffMin = diffSec / 60;
+      const diffHour = diffMin / 60;
+
+      // Helper function to correctly pluralize the time unit
+      const pluralize = (count, noun, suffix = "s") =>
+        `${count} ${noun}${count !== 1 ? suffix : ""}`;
+
+      if (diffSec < 60) {
+        return "Just now";
+      } else if (diffMin < 60) {
+        return pluralize(Math.floor(diffMin), "minute") + " ago";
+      } else if (diffHour < 24) {
+        return pluralize(Math.floor(diffHour), "hour") + " ago";
+      } else if (isYesterday(actualDate)) {
+        return "Yesterday";
+      } else {
+        return formatDistanceToNow(actualDate, { addSuffix: true });
+      }
     };
-  }, []);
+
+    console.log("Current Date: ", new Date());
+
+    const newNotification = {
+      id: uuid.v4(), // Use current timestamp as a unique ID
+      title,
+      timeStamp: displayTimeStamp(actualDate),
+      actualDate,
+      read: false,
+    };
+    // Update state by adding the new notification to the start of the list
+    await AsyncStorage.getItem("notifications").then((data) => {
+      const notifications = data ? JSON.parse(data) : [];
+      const updatedNotifications = [newNotification, ...notifications]; // Prepend new notification
+      AsyncStorage.setItem(
+        "notifications",
+        JSON.stringify(updatedNotifications)
+      ).then(() => {
+        console.log("Notification added and AsyncStorage updated.");
+        setNotifications(updatedNotifications);
+      });
+    });
+  };
 
   // Function to register for push notifications
   async function registerForPushNotificationsAsync() {
@@ -74,93 +136,123 @@ export const NotificationProvider = ({ children }) => {
     }
   }
 
-  // Function to add a notification
-  const addNotification = (title, rawTimeStamp) => {
-    console.log("Received raw timeStamp:", rawTimeStamp);
-
-    // Determine if the timestamp is in seconds (iOS) or milliseconds (Android), ensure the timestamp is in milliseconds
-    const timeStamp =
-      rawTimeStamp > 1000000000000 ? rawTimeStamp : rawTimeStamp * 1000;
-
-    const actualDate = new Date(timeStamp);
-    console.log("Normalized Date:", actualDate);
-
-    const displayTimeStamp = isToday(actualDate)
-      ? "Today"
-      : isYesterday(actualDate)
-      ? "Yesterday"
-      : formatDistanceToNow(actualDate, { addSuffix: true });
-
-    console.log("Current Date: ", new Date());
-
-    const newNotification = {
-      id: uuid.v4(), // Use current timestamp as a unique ID
-      title,
-      timeStamp: displayTimeStamp,
-      actualDate,
-      read: false,
-    };
-
-    setNotifications((prevNotifications) => {
-      const updatedNotifications = [newNotification, ...prevNotifications];
-      //for testData
-      return updatedNotifications.sort((a, b) => b.actualDate - a.actualDate); // Sort by date descending
-    });
-  };
-
   // // Test function to add notifications for different time scenarios
-  const testAddNotification = () => {
+  const testAddNotification = async () => {
     console.log("Testing notification addition");
     const currentTimestamp = new Date().getTime(); // Get current timestamp for "today"
     const testData = [
-      { title: "Test Today", date: currentTimestamp },
+      { title: "Test Just now", date: currentTimestamp },
+      {
+        title: "Test 1 min ago",
+        date: currentTimestamp - 60000,
+      }, // 1 min ago
+      {
+        title: "Test 2 mins ago",
+        date: currentTimestamp - 120000,
+      }, // 2 mins ago
+      {
+        title: "Test 59 mins ago",
+        date: currentTimestamp - 3550000,
+      }, // 29 mins ago
+      {
+        title: "Test 1 hour ago",
+        date: currentTimestamp - 3600000,
+      }, // 1 hour ago
+      {
+        title: "Test 23 hours ago",
+        date: currentTimestamp - 82800000,
+      }, // 23 hours ago
       { title: "Test Yesterday", date: currentTimestamp - 86400000 }, // 24 hours ago
       { title: "Test Two Days Ago", date: currentTimestamp - 172800000 }, // 48 hours ago
       {
-        title: "Test 1 month ago",
-        date: new Date("2024-04-01T18:12:49Z").getTime(),
-      },
-      {
         title: "Brisbane v.s Sydney @ 8 p.m. tonight!",
-        date: new Date("2024-03-01T18:12:49Z").getTime(),
+        date: new Date("2024-03-05T18:12:49Z").getTime(),
       },
       {
         title: "Season tickets on sale from Mar 25 to April 2!",
-        date: new Date("2024-02-25T00:00:00Z").getTime(),
+        date: new Date("2024-02-1T00:00:00Z").getTime(),
       },
       {
         title:
           "Welcome to Brisbane Bullets app! Don’t forget to turn on notifications~",
-        date: new Date("2024-01-25T00:00:00Z").getTime(),
+        date: new Date("2024-01-01T00:00:00Z").getTime(),
       },
     ];
 
-    testData.forEach((test) => {
-      const timeStamp =
-        test.date instanceof Date ? test.date.toISOString() : test.date;
-      addNotification(test.title, timeStamp);
+    //sort test data by date
+    testData.sort((a, b) => a.date - b.date);
+
+    for (const item of testData) {
+      await addNotification(item.title, item.date);
+    }
+  };
+
+  const markAsRead = async (ids) => {
+    // Ensure ids is always an array even if a single id is passed
+    if (!Array.isArray(ids)) {
+      ids = [ids];
+    }
+
+    console.log("Attempting to mark notifications as read with IDs:", ids);
+
+    setNotifications((currentNotifications) => {
+      const updatedNotifications = currentNotifications.map((notification) => {
+        if (ids.includes(notification.id)) {
+          console.log(
+            "Found notification with ID, updating read status:",
+            notification.id
+          );
+          return { ...notification, read: true };
+        }
+        return notification;
+      });
+
+      AsyncStorage.setItem(
+        "notifications",
+        JSON.stringify(updatedNotifications)
+      )
+        .then(() => {
+          console.log(
+            "AsyncStorage successfully updated with new read status."
+          );
+        })
+        .catch((error) => {
+          console.error("Failed to update AsyncStorage:", error);
+        });
+
+      return updatedNotifications;
     });
   };
 
-  // Function to mark notifications as read
-  const markAsRead = (ids) => {
-    setNotifications(
-      notifications.map((notification) =>
-        ids.includes(notification.id)
-          ? { ...notification, read: true }
-          : notification
+  const deleteNotifications = async (ids) => {
+    // If the ID is in an array, extract it to ensure proper comparison.
+    if (!Array.isArray(ids)) {
+      ids = [ids];
+    }
+
+    console.log("Attempting to delete notification with ID:", ids);
+
+    setNotifications((currentNotifications) => {
+      const filteredNotifications = currentNotifications.filter(
+        (notification) => !ids.includes(notification.id)
+      );
+
+      AsyncStorage.setItem(
+        "notifications",
+        JSON.stringify(filteredNotifications)
       )
-    );
+        .then(() => {
+          console.log("AsyncStorage successfully updated after deletion.");
+        })
+        .catch((error) => {
+          console.error("Failed to update AsyncStorage after deletion:", error);
+        });
+
+      return filteredNotifications;
+    });
   };
-  // Function to delete notifications
-  const deleteNotifications = (ids) => {
-    setNotifications(
-      notifications.filter((notification) => !ids.includes(notification.id))
-    );
-  };
-  // Function to get the count of unread notifications
-  const getUnreadCount = () =>
-    notifications.filter((notification) => !notification.read).length;
+
+  const getUnreadCount = () => notifications.filter((n) => !n.read).length;
 
   return (
     <NotificationContext.Provider
@@ -175,6 +267,16 @@ export const NotificationProvider = ({ children }) => {
     >
       {children}
       <Button title="Test Date Notifications" onPress={testAddNotification} />
+
+      <Button
+        title="Log Current Notifications"
+        onPress={() => {
+          console.log(notifications);
+          AsyncStorage.getItem("notifications").then((data) => {
+            console.log("AsyncStorage:", JSON.parse(data));
+          });
+        }}
+      />
     </NotificationContext.Provider>
   );
 };
