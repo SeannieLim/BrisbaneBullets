9b71890f01d8fb6014957c2cafc78f648ea10732
Arinexus <arin.ning02@gmail.com>
2024-05-08 09:57:35 +1000

    change username in config

--- BrisbaneBullets/app.config.js
+++ BrisbaneBullets/app.config.js
@@ -2,12 +2,14 @@ import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
-  owner: "arinning", //Have to hardcode this value
   extra: {
     ...config.extra,
     eas: {
+      //have to hard code this at the moment
       projectId: "3e9ffb5d-1cae-4cb6-bdab-3d8d00971a21",
     },
-    expoUsername: process.env.EXPO_USERNAME,
+
+    //change this into your expo username or not idk haha
+    expoUsername: "four-eyes",
   },
 });
