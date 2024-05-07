c8c676c4e096a45b78be396ec5e6f8558f79758c
Amber <60221403+amber871023@users.noreply.github.com>
2024-05-08 02:03:08 +1000

    modify NewsCard and NewsScreen
    1. change NewsCard based on BBS' feedback: enlarge image size
    2. change NewsScreen to make it look similar to AllVideoScreen

--- BrisbaneBullets/src/screens/NewsScreen.js
+++ BrisbaneBullets/src/screens/NewsScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Box } from "@gluestack-ui/themed";
-import { Dimensions, StyleSheet, FlatList } from "react-native";
-import NewsCard from "../components/NewsCard";
+import { View, Box, Image, VStack, Text, HStack } from "@gluestack-ui/themed";
+import { Dimensions, StyleSheet, FlatList, TouchableOpacity, Share } from "react-native";
 import AdsBanner from "../components/AdsBanner";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 const windowWidth = Dimensions.get("window").width;
 const mockNews = [
@@ -57,22 +58,69 @@ const mockNews = [
   },
 ];
 export default function NewsScreen({ navigation }) {
-  const handleCardPress = (newsId) => {
+  const handleCardPress = (id) => {
     navigation.navigate('NewsDetail', { newsId: id });
+  };
+  const handleShare = async (news) => {
+    try {
+      // Base URL
+      const baseUrl = 'https://www.brisbanebullets.com.au/news/';
+      // Replace spaces in the news title with hyphens (-)
+      const formattedTitle = news.title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+      // Construct the final URL
+      const url = `${baseUrl}${formattedTitle}`;
+      // Share the URL
+      await Share.share({
+        url: url,
+      });
+
+    } catch (error) {
+      console.error("Error sharing", error);
+    }
   };
 
   const renderNewsItem = ({ item, index }) => (
     <React.Fragment key={item.id}>
       {/* Render NewsCard */}
-      <Box my={10}>
-        <NewsCard key={item.id} news={item} onPress={() => handleCardPress(item.id)} />
-      </Box>
-      {index > 0 && (index + 1) % 3 === 0 && (
-        <AdsBanner
-          imageUrl="https://www.interprint-services.co.uk/wp-content/uploads/2019/04/placeholder-banner.png"
-        />
-      )}
-    </React.Fragment>
+      <TouchableOpacity onPress={() => handleCardPress(item.id)} my={5}>
+        <View mt={5} mb={10} style={styles.newsContainer}>
+          <Box style={styles.newsImg}>
+            <Image
+              source={item.img}
+              alt={item.imgAlt}
+              w={'100%'}
+              h={'100%'}
+              overflow='hidden'
+              borderTopRightRadius={15}
+              borderTopLeftRadius={15}
+              position='relative'
+            />
+            <HStack alignItems='flex-end' justifyContent='space-between' px={10} mb={5} position='absolute' bottom={0} left={0} right={0}>
+              <HStack alignItems="center" borderRadius={15} px={10} py={5} backgroundColor='rgba(60, 60, 60, .7)'>
+                <FontAwesome5 name="calendar-alt" size={14} color="white" />
+                <Text pl={5} color='white' fontSize={12}>{item.date}</Text>
+              </HStack>
+              <TouchableOpacity style={styles.shareIcon} onPress={() => handleShare(item)}>
+                <Entypo name="share-alternative" size={18} color="white" />
+              </TouchableOpacity>
+            </HStack>
+          </Box>
+          <VStack style={styles.description} py={10} px={15}>
+            <Text fontWeight='bold' numberOfLines={2} ellipsizeMode="tail">
+              {item.title}
+            </Text>
+          </VStack>
+        </View>
+      </TouchableOpacity>
+
+      {
+        index > 0 && (index + 1) % 3 === 0 && (
+          <AdsBanner
+            imageUrl="https://www.interprint-services.co.uk/wp-content/uploads/2019/04/placeholder-banner.png"
+          />
+        )
+      }
+    </React.Fragment >
   );
 
   return (
@@ -95,6 +143,29 @@ const styles = StyleSheet.create({
   },
   flatListContent: {
     paddingVertical: 10,
-    paddingHorizontal: 5,
   },
+  newsImg: {
+    width: "100%",
+    height: windowWidth * 0.38,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  description: {
+    width: "100%",
+    height: windowWidth * 0.15,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    backgroundColor: "#F5F5F5",
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  shareIcon: {
+    borderRadius: 20,
+    backgroundColor: 'rgba(60, 60, 60, .7)',
+    padding: 8,
+  },
+
 });
