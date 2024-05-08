72e6f487147caa2ddf8ea027b37c2e6793e8a5fb
Nguyen Hao Tran <nguyen4754@gmail.com>
2024-05-08 23:26:39 +1000

    Merge branch 'teamSean/Neil/GifHover' into preview

--- BrisbaneBullets/src/screens/AdvanceStatsWebView.js
+++ BrisbaneBullets/src/screens/AdvanceStatsWebView.js
@@ -1,5 +1,5 @@
 import React, { useRef, useLayoutEffect } from "react";
-import B_WebViewTemplate from "../components/b-WebViewTemplate";
+import B_WebViewTemplate from "../components/B_WebViewTemplate";
 
 const AdvanceStatsWebview = () => {
   return <B_WebViewTemplate title="Advance Stats" uri="https://www.brisbanebullets.com.au/Statistics" />;
