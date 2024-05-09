1fcc33cca958cce1f616f97473f3c7f2dda80491
Arinexus <arin.ning02@gmail.com>
2024-05-09 21:05:01 +1000

    hide headerbacktitle for all video screen

--- BrisbaneBullets/src/navigation/HomeStack.js
+++ BrisbaneBullets/src/navigation/HomeStack.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {createStackNavigator} from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import B_StoreScreen from "../screens/B-StoreScreen";
 import LiveScreen from "../screens/LiveScreen";
@@ -7,79 +7,96 @@ import StandingsScreen from "../screens/StandingsScreen";
 import NewsScreen from "../screens/NewsScreen";
 import NewsDetailScreen from "../screens/NewsDetailScreen";
 import AllVideos from "../screens/AllVideos";
-import {Ionicons} from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeStack = createStackNavigator();
 
 function HomeStackScreen() {
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{headerShown: false}}
-            />
-            <HomeStack.Screen
-                name="LiveScreen"
-                component={LiveScreen}
-                options={{headerShown: true}}
-            />
-            <HomeStack.Screen
-                name="B-StoreScreen"
-                component={B_StoreScreen}
-                options={{headerShown: true}}
-            />
-            <HomeStack.Screen
-                name="News"
-                component={NewsScreen}
-                options={{
-                    headerStyle: {
-                        backgroundColor: 'white',
-                    },
-                    headerTitleStyle: {
-                        fontSize: 20,
-                    },
-                    headerTintColor: '#113B81',
-                    headerBackImage: () => (
-                        <Ionicons name="chevron-back-outline" size={27} color="#FAB81B" pl={4}/>
-                    ),
-                    headerBackTitleVisible: false,
-                }}
-            />
-            <HomeStack.Screen
-                name="NewsDetail"
-                component={NewsDetailScreen}
-                options={{
-                    headerStyle: {
-                        backgroundColor: '#113B81',
-                    },
-                    headerTitleStyle: {
-                        fontSize: 20,
-                    },
-                    headerTintColor: 'white',
-                    headerBackImage: () => (
-                        <Ionicons name="chevron-back-outline" size={27} color="#FAB81B" pl={4}/>
-                    ),
-                    headerBackTitleVisible: false,
-                }}/>
-          <HomeStack.Screen
-        name="Videos"
-        component={AllVideos}
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="LiveScreen"
+        component={LiveScreen}
+        options={{ headerShown: true }}
+      />
+      <HomeStack.Screen
+        name="B-StoreScreen"
+        component={B_StoreScreen}
+        options={{ headerShown: true }}
+      />
+      <HomeStack.Screen
+        name="News"
+        component={NewsScreen}
         options={{
           headerStyle: {
-            backgroundColor: 'white',
+            backgroundColor: "white",
           },
           headerTitleStyle: {
             fontSize: 20,
           },
-          headerTintColor: '#113B81',
+          headerTintColor: "#113B81",
           headerBackImage: () => (
-            <Ionicons name="chevron-back-outline" size={27} color="#FAB81B" pl={4} />
+            <Ionicons
+              name="chevron-back-outline"
+              size={27}
+              color="#FAB81B"
+              pl={4}
+            />
           ),
+          headerBackTitleVisible: false,
+        }}
+      />
+      <HomeStack.Screen
+        name="NewsDetail"
+        component={NewsDetailScreen}
+        options={{
+          headerStyle: {
+            backgroundColor: "#113B81",
+          },
+          headerTitleStyle: {
+            fontSize: 20,
+          },
+          headerTintColor: "white",
+          headerBackImage: () => (
+            <Ionicons
+              name="chevron-back-outline"
+              size={27}
+              color="#FAB81B"
+              pl={4}
+            />
+          ),
+          headerBackTitleVisible: false,
+        }}
+      />
+      <HomeStack.Screen
+        name="Videos"
+        component={AllVideos}
+        options={{
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerTitleStyle: {
+            fontSize: 20,
+          },
+          headerTintColor: "#113B81",
+          headerBackImage: () => (
+            <Ionicons
+              name="chevron-back-outline"
+              size={27}
+              color="#FAB81B"
+              pl={4}
+            />
+          ),
+          headerBackTitleVisible: false,
         }}
       />
     </HomeStack.Navigator>
-    );
+  );
 }
 
 export default HomeStackScreen;
