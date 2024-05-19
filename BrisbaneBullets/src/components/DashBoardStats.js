1710765b3963508d27f8d85b795052b0115ddd11
Nguyen Hao Tran <nguyen4754@gmail.com>
2024-05-19 14:39:40 +1000

    Attempt 2
    Success!

--- BrisbaneBullets/src/components/DashBoardStats.js
+++ BrisbaneBullets/src/components/DashBoardStats.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { scaleFontSize } from "../constants/Layout";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const DashBoardStatsRound1 = () => {
+const DashBoardStats = () => {
     return (
         <View>
             <View style = {styles.DashBoardBox}>
@@ -77,20 +78,43 @@ const DashBoardStatsRound1 = () => {
             </View>
             <View style={[styles.bigStatsBanner, {flexDirection: 'row'}]}>
                 <View style={{ flex: 1 }}>
-                    <Text style={[MyStyles.bannerNumber, { textAlign: 'center', marginTop: windowHeight * 0.02 }]}>20</Text>
-                    <Text style={[MyStyles.bannerText, { textAlign: 'center', marginTop: 5 }]}>Rebounds</Text>                
+                    <Text style={[styles.bannerNumber, { marginTop: windowHeight * 0.02 }]}>20</Text>
+                    <Text style={[styles.bannerText]}>Rebounds</Text>                
                 </View>
                 <View style={{ flex: 1 }}>
-                    <Text style={[MyStyles.bannerNumber, { textAlign: 'center', marginTop: windowHeight * 0.02 }]}>30</Text>
-                    <Text style={[MyStyles.bannerText, { textAlign: 'center', marginTop: 5 }]}>Assists</Text>                
+                    <Text style={[styles.bannerNumber, {marginTop: windowHeight * 0.02 }]}>30</Text>
+                    <Text style={[styles.bannerText]}>Assists</Text>                
                 </View>
                 <View style={{ flex: 1 }}>
-                    <Text style={[MyStyles.bannerNumber, { textAlign: 'center', marginTop: windowHeight * 0.02 }]}>10</Text>
-                    <Text style={[MyStyles.bannerText, { textAlign: 'center', marginTop: 5 }]}>Steals</Text>                
+                    <Text style={[styles.bannerNumber, { marginTop: windowHeight * 0.02 }]}>10</Text>
+                    <Text style={[styles.bannerText]}>Steals</Text>                
                 </View>
             </View>
             <View style={[styles.circleRow, {flexDirection: 'row'}]}>
-
+                <View style={styles.circleContainer}>
+                    <View style={styles.circle}>
+                        <Text style={styles.circleText}>90.0</Text>
+                    </View>
+                    <Text style={styles.circleLabel}>FG%</Text>
+                </View>
+                <View style={styles.circleContainer}>
+                    <View style={styles.circle}>
+                        <Text style={styles.circleText}>90.0</Text>
+                    </View>
+                    <Text style={styles.circleLabel}>3PTS%</Text>
+                </View>
+                <View style={styles.circleContainer}>
+                    <View style={styles.circle}>
+                        <Text style={styles.circleText}>100.0</Text>
+                    </View>
+                    <Text style={styles.circleLabel}>2PTS%</Text>
+                </View>
+                <View style={styles.circleContainer}>
+                    <View style={styles.circle}>
+                        <Text style={styles.circleText}>90.0</Text>
+                    </View>
+                    <Text style={styles.circleLabel}>FT%</Text>
+                </View>
             </View>
         </View>
     )
@@ -98,38 +122,93 @@ const DashBoardStatsRound1 = () => {
 
 const styles = StyleSheet.create({
     DashBoardBox: {
-        marginLeft: windowWidth * 0.08,
-        marginTop: windowWidth * 0.05,
+        flexGrow: 1,
+        marginLeft: -windowWidth * 0.03,
+        marginRight: windowWidth * 0.04,
+        marginTop: windowWidth * 0.06,
+        backgroundColor: "#FFFFFF",
+        borderRadius: 20,
     },
     statsText: {
         fontSize: scaleFontSize(18),
-        color: "#000000",
+        color: "#164CA8",
         fontWeight: "bold",
         letterSpacing: 1,
         alignItems: "center",
+        marginLeft: -windowWidth * 0.03
     },
     statsNumber: {
-        fontSize: scaleFontSize(18),
-        color: "#000000",
-        fontWeight: "bold",
+        fontSize: scaleFontSize(16),
+        color: "#164CA8",
         letterSpacing: 1,
         alignItems: "center",
+        marginLeft: -windowWidth * 0.03
     },
     teamIcon: {
-        width: 13, // Set width of the icon
-        height: 13, // Set height of the icon
-        marginLeft: 2, // Add some space between icon and text
+        width: 35, // Set width of the icon
+        height: 35, // Set height of the icon
+        marginTop: windowHeight * 0.013,
+        marginLeft: windowWidth * 0.05,
+        marginBottom: windowHeight *0.01,
+        alignItems: "center",
     },
     bigStatsBanner: {
         width: windowWidth,
-        marginTop: 150,
+        flexGrow: 1,
+        marginLeft: -windowWidth * 0.08,
+        marginRight: windowWidth * 0.04,
+        marginTop: windowWidth * 0.1,
+        backgroundColor: "#164CA8",
     },
     bannerNumber: {
-        fontSize: scaleFontSize(22),
+        marginBottom: -windowHeight * 0.0019,
+        fontSize: scaleFontSize(30),
+        color: "#FFFFFF",
+        textAlign: 'center',
+        letterSpacing: 1,
     },
     bannerText: {
-        fontSize: scaleFontSize(22),
+        fontSize: scaleFontSize(16),
+        textAlign: "center",
+        color: "#FFFFFF",
+        marginTop: windowHeight * 0.0001,
+        textAlign: 'center',
+        letterSpacing: 1.1,
+        marginBottom: windowHeight *0.02,
     },
+    circleRow: {
+        marginTop: 20,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexGrow: 1,
+        marginLeft: -windowWidth * 0.08,
+        marginBottom: windowHeight * 0.15
+    },
+    circleContainer: {
+        alignItems: 'center',
+    },
+    circle: {
+        width: windowWidth * 0.18,
+        height: windowHeight * 0.08,
+        borderRadius: 100,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: windowHeight * 0.02,
+    },
+    circleText: {
+        fontSize: scaleFontSize(18),
+        color: '#000000',
+        fontWeight: 'bold',
+    },
+    circleLabel: {
+        marginTop: windowHeight * 0.014,
+        marginLeft: windowWidth * 0.02,
+        fontSize: scaleFontSize(14),
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
 })
 
-export default DashBoardStatsRound1;
+export default DashBoardStats;
