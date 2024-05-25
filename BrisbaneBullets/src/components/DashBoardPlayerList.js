ae95308aba25fe36a16ce32c6e5dc561ca41d8c7
Nguyen Hao Tran <nguyen4754@gmail.com>
2024-05-26 00:14:41 +1000

    changed playersquares
    success maybe

--- BrisbaneBullets/src/components/DashBoardPlayerList.js
+++ BrisbaneBullets/src/components/DashBoardPlayerList.js
@@ -17,133 +17,178 @@ const players = [
   {
     id: "1",
     jerseyNumber: 11,
-    playerName: "Rocco Zikarsky",
+    playerName: "Rocco",
     playerImage: require("../../assets/teamPageImages/RoccoZikarsky.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/RoccoZikarskyHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 3,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "2",
     jerseyNumber: 12,
-    playerName: "Aron Baynes",
+    playerName: "Aron",
     playerImage: require("../../assets/teamPageImages/AronBaynes.jpeg"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/AronBaynesHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 2,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "3",
     jerseyNumber: 23,
-    playerName: "Casey Prather",
+    playerName: "Casey",
     playerImage: require("../../assets/teamPageImages/CaseyPrather.jpeg"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 2,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "4",
     jerseyNumber: 34,
-    playerName: "Chris Smith",
+    playerName: "Chris",
     playerImage: require("../../assets/teamPageImages/ChrisSmith.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/ChrisSmithHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 1,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "5",
     jerseyNumber: 0,
-    playerName: "DJ Mitchell",
+    playerName: "DJ",
     playerImage: require("../../assets/teamPageImages/DJMitchell.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/DJMitchellHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "6",
     jerseyNumber: 4,
-    playerName: "Gabe Hadley",
+    playerName: "Gabe",
     playerImage: require("../../assets/teamPageImages/GabeHadley.jpeg"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/GabeHadleyHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "7",
     jerseyNumber: 2,
-    playerName: "Isaac White",
+    playerName: "Isaac",
     playerImage: require("../../assets/teamPageImages/IsaacWhite.jpeg"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/IsaacWhiteHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "8",
     jerseyNumber: 13,
-    playerName: "Josh Bannan",
+    playerName: "Josh",
     playerImage: require("../../assets/teamPageImages/JoshBannan.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "9",
     jerseyNumber: 8,
-    playerName: "Mitch Norton",
+    playerName: "Mitch",
     playerImage: require("../../assets/teamPageImages/MitchNorton.jpeg"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/MitchNortonHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "10",
     jerseyNumber: 20,
-    playerName: "Nathan Sobey",
+    playerName: "Nathan",
     playerImage: require("../../assets/teamPageImages/NathanSobey.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/NathanSobeyHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "11",
     jerseyNumber: 26,
-    playerName: "Sam McDaniel",
+    playerName: "Sam",
     playerImage: require("../../assets/teamPageImages/SamMcDaniel.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/SamMcDanielHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "12",
     jerseyNumber: 32,
-    playerName: "Matthew Johns",
+    playerName: "Matthew",
     playerImage: require("../../assets/teamPageImages/MatthewJohns.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/MattJohnsHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "13",
     jerseyNumber: 3,
-    playerName: "Shannon Scott",
+    playerName: "Shannon",
     playerImage: require("../../assets/teamPageImages/ShannonScott.jpeg"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
     playerHeadShot: require("../../assets/teamPageImages/ShannonScottHead.jpg"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "14",
     jerseyNumber: 6,
-    playerName: "Tristan Devers",
+    playerName: "Tristan",
     playerImage: require("../../assets/teamPageImages/TristanDevers.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   },
   {
     id: "15",
     jerseyNumber: 24,
-    playerName: "Tyrell Harrison",
+    playerName: "Tyrell",
     playerImage: require("../../assets/teamPageImages/TyrrelHarrison.png"),
     playerProfileImage: require("../../assets/teamPageImages/Image1.png"),
-    playerDashboard: require("../../assets/Dashboard/playerStats.png")
+    playerDashboard: require("../../assets/Dashboard/playerStats.png"),
+    points: 0,
+    assists: 2,
+    rebounds: 1
   }
 ];
 
@@ -155,17 +200,29 @@ const Square = ({ player }) => {
 
   return (
     <TouchableOpacity onPress={navigateToProfile}>
-      <View style={styles.square}>
-        <View style={styles.imageContainer}>
-          <Image source={player.playerImage} style={styles.playerImage} />
+      <View style={styles.wrapper}>
+        <View style={styles.square}>
+          <View style={styles.imageContainer}>
+            <Image source={player.playerImage} style={styles.playerImage} />
+          </View>
         </View>
         <View style={styles.textContainer}>
-          <Text style={styles.jerseyNumber}>{player.jerseyNumber}</Text>
+          <Text style={styles.playerName}>{player.playerName}</Text>
+        </View>
+        <View style={styles.smallStatsContainer}>
+          <Text style={styles.smallStats}>assist 2 | rebound 1</Text>
+        </View>
+        <View style={styles.bigStatsContainer}>
+          <Text style={styles.bigStats}>{player.points}</Text>
+        </View>
+        <View style={styles.pointContainer}>
+          <Text style={styles.pointText}>pt</Text>
         </View>
       </View>
     </TouchableOpacity>
   );
 };
+
 
 const DashBoardPlayerList = () => {
   return (
@@ -173,11 +230,10 @@ const DashBoardPlayerList = () => {
     <View style={styles.container}>
       <FlatList
         data={players}
-        renderItem={({ item }) => <Square player={item} />}
+        renderItem={({ item }) => <Square player={item}/>}
         keyExtractor={(item) => item.id}
-        numColumns={2}
+        numColumns={1}
         showsVerticalScrollIndicator={false}
-        columnWrapperStyle={styles.columnWrapper}
       />
     </View>
 
@@ -190,44 +246,82 @@ const styles = StyleSheet.create({
   container: {
     paddingBottom: windowHeight * 0.5,
   },
+  wrapper: {
+    height: windowHeight * 0.11,
+    width: windowWidth * 0.85,
+    marginTop: windowHeight * 0.02,
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    borderRadius: 30,
+  },
   columnWrapper: {
     justifyContent: "space-between",
     marginTop: windowHeight * 0.01,
     marginHorizontal: windowWidth * 0.01,
   },
   square: {
-    width: 140,
-    height: 120,
-    backgroundColor: "lightblue",
-    marginVertical: windowHeight * 0.01,
-    borderRadius: 15,
+    height: windowHeight * 0.11,
+    width: windowWidth * 0.35,
+    borderTopLeftRadius: 30,
+    borderBottomLeftRadius: 30,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
     shadowColor: "#000",
     shadowOffset: { width: 1, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
     overflow: "hidden",
-    marginRight : windowWidth * 0.1,
   },
   imageContainer: {
-    width: "100%",
+    width: "95%",
     height: "100%",
-    borderRadius: 15,
     overflow: "hidden",
   },
   playerImage: {
-    width: "100%",
+    width: "95%",
     height: "100%",
     resizeMode: "cover",
   },
   textContainer: {
     position: "absolute",
-    top: 15,
-    left: 10,
-    alignItems: "center",
+    marginTop: windowHeight * 0.011,
+    marginLeft: windowWidth * 0.35,
   },
-  jerseyNumber: {
+  playerName: {
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 20,
+    color: "#113B81",
+    letterSpacing: 0.5,
+  },
+  smallStatsContainer: {
+    position: "absolute",
+    marginTop: windowHeight * 0.07,
+    marginLeft: windowWidth * 0.35,
+  },
+  smallStats: {
+    fontSize: 12,
+    color: "#113B81",
+    letterSpacing: 0.5,
+  },
+  bigStatsContainer: {
+    position: "absolute",
+    marginTop: windowHeight * 0.014,
+    marginLeft: windowWidth * 0.7,
+  },
+  bigStats: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#113B81",
+    letterSpacing: 0.5,
+  },
+  pointContainer: {
+    position: "absolute",
+    marginTop: windowHeight * 0.055,
+    marginLeft: windowWidth * 0.78,
+  },
+  pointText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#113B81",
   },
 });
